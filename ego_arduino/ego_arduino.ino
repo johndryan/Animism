a8058c3527307923bb0a92e8cba4fc2234ebb233
John Ryan <john@lifeforms.ie>
2012-03-25 16:37:16 -0700

    Two-motor Movement
    Robot chassis now has two separate motors to drive left and right wheels.

--- ego_arduino/ego_arduino.ino
+++ ego_arduino/ego_arduino.ino
@@ -1,5 +1,7 @@
-int MotorPinDirection = 13;
-int MotorPinSpeed = 11;
+int MotorOneDirection = 13;
+int MotorOneSpeed = 11;
+int MotorTwoDirection = 12;
+int MotorTwoSpeed = 3;
 int sensorVal = 300;
 int sensorPin = 5;
 int maxDistance = 200;
@@ -7,26 +9,62 @@ int minDistance = 500;
 
 void setup() {
   //Serial.begin(9600);
-  pinMode (MotorPinDirection, OUTPUT);
-  pinMode (MotorPinSpeed, OUTPUT);
+  pinMode (MotorOneDirection, OUTPUT);
+  pinMode (MotorOneSpeed, OUTPUT);
+  pinMode (MotorTwoDirection, OUTPUT);
+  pinMode (MotorTwoSpeed, OUTPUT);
 }
 
 void loop(){
-  sensorVal = analogRead(sensorPin);
-  delay(1);
-  if (sensorVal < maxDistance) {
-    // FORWARD
-    digitalWrite(MotorPinDirection, LOW);
-    analogWrite(MotorPinSpeed, 200);
-  } else if (sensorVal > minDistance) {
-    // REVERSE
-    digitalWrite(MotorPinDirection, HIGH);
-    analogWrite(MotorPinSpeed, 200);
-  } else {
-    //Serial.println("STOP");
-    digitalWrite(MotorPinDirection, LOW);
-    analogWrite(MotorPinSpeed, 0);
-  }
-  //Serial.println(sensorVal);
+  forward();
+  delay(2000);
+  stop();
+  delay(500);
+  left();
+  delay(2000);
+  stop();
+  delay(500);
+  backward();
+  delay(2000);
+  stop();
+  delay(500);
+  right();
+  delay(2000);
+  stop();
+  delay(500);
 }
 
+void forward() {
+    digitalWrite(MotorOneDirection, HIGH);
+    analogWrite(MotorOneSpeed, 255);
+    digitalWrite(MotorTwoDirection, HIGH);
+    analogWrite(MotorTwoSpeed, 255);
+}
+
+void left() {
+    digitalWrite(MotorOneDirection, LOW);
+    analogWrite(MotorOneSpeed, 255);
+    digitalWrite(MotorTwoDirection, HIGH);
+    analogWrite(MotorTwoSpeed, 255);
+}
+
+void right() {
+    digitalWrite(MotorOneDirection, HIGH);
+    analogWrite(MotorOneSpeed, 255);
+    digitalWrite(MotorTwoDirection, LOW);
+    analogWrite(MotorTwoSpeed, 255);
+}
+
+void backward() {
+    digitalWrite(MotorOneDirection, HIGH);
+    analogWrite(MotorOneSpeed, 255);
+    digitalWrite(MotorTwoDirection, HIGH);
+    analogWrite(MotorTwoSpeed, 255);
+}
+
+void stop() {
+    digitalWrite(MotorOneDirection, LOW);
+    analogWrite(MotorOneSpeed, 0);
+    digitalWrite(MotorTwoDirection, LOW);
+    analogWrite(MotorTwoSpeed, 0);
+}
