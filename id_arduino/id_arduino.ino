4de97cb7d475053e9aae44116a9b85f1c2a9ced0
John Ryan <john@lifeforms.ie>
2012-03-25 16:49:55 -0700

    Light Seeking Servo Robot
    A second small robot, the I.D., runs on two continuos servo motors. Adapting plans and code for a Arduino Controlled Servo Robot (http://www.oomlout.com/a/products/serb/) I created a small and fast device that seeks out light

---
+++ id_arduino/id_arduino.ino
@@ -0,0 +1,166 @@
+/*
+ *  Based on Arduino Controlled Servo Robot (SERB) code: http://www.oomlout.com/serb 
+ */ 
+ 
+//---------------------------------------------------------------------------------
+// START OF ARDUINO CONTROLLED SERVO ROBOT (SERB) PREAMBLE
+#include <Servo.h>
+
+#define LEFTSERVOPIN  10
+#define RIGHTSERVOPIN  9
+#define rightLightSensor  2
+#define leftLightSensor  1
+#define proxSensor 3
+
+Servo leftServo; 
+Servo rightServo; 
+
+int speed = 50; //sets the speed of the robot (both servos) a percentage between 0 and 100
+int servoLeftOff = 90;
+int servoRightOff = 95;
+
+int SensorLeft;
+int SensorRight;
+int SensorDifference;
+
+int minDistance = 400;
+int lightDiff = 125;
+int proxVal = 500;
+
+// END OF ARDUINO CONTROLLED SERVO ROBOT (SERB) PREAMBLE
+//---------------------------------------------------------------------------------
+
+
+/*
+ * sets pins to appropriate states and attaches servos. Then pauses
+ * for 1 second before the program starts
+*/ 
+void setup()                  
+{
+  serbSetup();                       //sets the state of all neccesary pins and adds servos to your sketch
+  randomSeed(analogRead(0));         //sets the random number seed with something mildly random
+  delay(1000);
+}
+
+/*
+ * turns the robot either left or right (randomly) for a period between 
+ * 0.1 and 1 second. Before going forward for a random time period 
+ * between 1 and 4 seconds. Before pausing for two seconds then starting
+ * again.
+*/
+void loop()                     
+{
+  proxVal = analogRead(proxSensor);
+  delay(1);
+  SensorLeft = 1023 - analogRead(leftLightSensor); // This reads the value of the sensor, then saves it to the corresponding integer.
+  delay(1);
+  SensorRight = 1023 - analogRead(rightLightSensor); // This reads the value of the sensor, then saves it to the corresponding integer.
+  delay(1);
+  SensorDifference = abs(SensorLeft - SensorRight); // This calculates the difference between the two sensors and then saves it to an integer.
+  
+  if (proxVal > minDistance) {
+    goStop();
+  } else {
+    if (SensorLeft > SensorRight && SensorDifference > lightDiff) goRight();
+    if (SensorLeft < SensorRight && SensorDifference > lightDiff) goLeft();
+    if (SensorDifference < lightDiff) goForward();
+  }
+  
+//   turnRandom(100,1000);            //Turns randomly left or right for a random time period between .1 second and one second
+//   goForwardRandom(1000,2000);      //Goes forward for a random time period between 1 and 2 seconds
+//   goStop();                        //Stops the robot
+//   delay(2000);                     //pauses for 2 seconds (whilst stopped)
+}
+
+/*
+ * turns the robot randomly left or right for a random time period between
+ * minTime (milliseconds) and maxTime (milliseconds)
+ */
+void turnRandom(int minTime, int maxTime){
+  int choice = random(2);                     //Random number to decide between left (1) and right (0)
+  int turnTime = random(minTime,maxTime);     //Random number for the pause time
+  if(choice == 1){ goLeft();}                 //If random number = 1 then turn left
+  else {goRight();}                           //If random number = 0 then turn right
+  delay(turnTime);                            //delay for random time                         
+}
+
+/*
+ * goes forward for a random time period between minTime (milliseconds)
+ * and maxTime (milliseconds)
+ */
+void goForwardRandom(int minTime, int maxTime){
+  int forwardTime = random(minTime,maxTime);      //determine a random time to go forward
+  goForward();                                    //sets the SERB forward
+  delay(forwardTime);                             //delays for random time period
+}
+
+//------------------------------------------------------------------------------------------------------------
+//START OF ARDUINO CONTROLLED SERVO ROBOT (SERB) ROUTINES
+
+/*
+ * sets up your arduino to address your SERB using the included routines
+*/
+void serbSetup(){
+  setSpeed(speed);
+  pinMode(LEFTSERVOPIN, OUTPUT);     //sets the left servo signal pin to output
+  pinMode(RIGHTSERVOPIN, OUTPUT);    //sets the right servo signal pin to output
+  leftServo.attach(LEFTSERVOPIN);    //attaches left servo
+  rightServo.attach(RIGHTSERVOPIN);  //attaches right servo
+  pinMode(rightLightSensor, INPUT);
+  pinMode(leftLightSensor, INPUT);
+  goStop();
+}
+
+
+/*
+ * sets the speed of the robot between 0-(stopped) and 100-(full speed)
+ * NOTE: speed will not change the current speed you must change speed 
+ * then call one of the go methods before changes occur.
+*/ 
+void setSpeed(int newSpeed){
+  if(newSpeed >= 100) {newSpeed = 100;}     //if speed is greater than 100 make it 100
+  if(newSpeed <= 0) {newSpeed = 0;}         //if speed is less than 0 make it 0 
+  speed = newSpeed * 0.9;                   //scales the speed to be between 0 and 90
+}
+
+/*
+ * sends the robot forwards
+ */
+void goForward(){
+ leftServo.write(servoLeftOff + speed);
+ rightServo.write(servoRightOff - speed);
+}
+
+/*
+ * sends the robot backwards
+ */
+void goBackward(){
+ leftServo.write(servoLeftOff - speed);
+ rightServo.write(servoRightOff + speed);
+}
+  
+/*
+ * sends the robot right
+ */
+void goRight(){
+ leftServo.write(servoLeftOff + speed);
+ rightServo.write(servoRightOff + speed);
+}
+
+/*
+ * sends the robot left
+ */
+void goLeft(){
+ leftServo.write(servoLeftOff - speed);
+ rightServo.write(servoRightOff - speed);
+}
+
+/*
+ * stops the robot
+ */
+void goStop(){
+ leftServo.write(servoLeftOff);
+ rightServo.write(servoRightOff);
+} 
+//END OF ARDUINO CONTROLLED SERVO ROBOT (SERB) ROUTINES
+//------------------------------------------------------------------------------------------------------------
