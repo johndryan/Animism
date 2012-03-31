361413372f34cb76adb4ee8be2b949ced3327e58
John Ryan <john@lifeforms.ie>
2012-03-31 12:30:45 -0700

    Code tidying and commenting

--- id_arduino/id_arduino.ino
+++ id_arduino/id_arduino.ino
@@ -1,10 +1,17 @@
 /*
- *  Based on Arduino Controlled Servo Robot (SERB) code: http://www.oomlout.com/serb 
+ *  Based on SERB code from here: http://www.oomlout.com/serb 
  */ 
- 
+
+// LIBRARIES
 //---------------------------------------------------------------------------------
-// START OF ARDUINO CONTROLLED SERVO ROBOT (SERB) PREAMBLE
+
 #include <Servo.h>
+
+Servo leftServo;
+Servo rightServo;
+
+// DEFINE PINS
+//--------------------------------------------------------------------------------- 
 
 #define LEFTSERVOPIN  10
 #define RIGHTSERVOPIN  9
@@ -12,29 +19,28 @@
 #define leftLightSensor  1
 #define proxSensor 3
 
-Servo leftServo; 
-Servo rightServo; 
-
-int speed = 50; //sets the speed of the robot (both servos) a percentage between 0 and 100
-int servoLeftOff = 90;
-int servoRightOff = 90;
+// MISC VARS
+//--------------------------------------------------------------------------------- 
 
 int SensorLeft;
 int SensorRight;
 int SensorDifference;
-
-int minDistance = 400;
-int lightDiff = 125;
 int proxVal = 500;
 
-// END OF ARDUINO CONTROLLED SERVO ROBOT (SERB) PREAMBLE
+// SETTINGS
+//--------------------------------------------------------------------------------- 
+
+int speed = 50;             // sets the speed of the robot (both servos) between 0 and 90
+int servoLeftOff = 90;      // If left servo can't be adjusted manually, set the stop value here
+int servoRightOff = 90;     // If right servo can't be adjusted manually, set the stop value here
+
+int minDistance = 400;      // proximity is triggered above this
+int lightDiff = 125;        // sensitivity in difference between light sensors
+
+
+// SETUP
 //---------------------------------------------------------------------------------
 
-
-/*
- * sets pins to appropriate states and attaches servos. Then pauses
- * for 1 second before the program starts
-*/ 
 void setup()                  
 {
   serbSetup();                       //sets the state of all neccesary pins and adds servos to your sketch
@@ -42,21 +48,18 @@ void setup()
   delay(1000);
 }
 
-/*
- * turns the robot either left or right (randomly) for a period between 
- * 0.1 and 1 second. Before going forward for a random time period 
- * between 1 and 4 seconds. Before pausing for two seconds then starting
- * again.
-*/
+// LOOP
+//--------------------------------------------------------------------------------- 
+
 void loop()                     
 {
   proxVal = analogRead(proxSensor);
   delay(1);
-  SensorLeft = 1023 - analogRead(leftLightSensor); // This reads the value of the sensor, then saves it to the corresponding integer.
+  SensorLeft = 1023 - analogRead(leftLightSensor);        // This reads the value of the sensor, then saves it to the corresponding integer.
   delay(1);
-  SensorRight = 1023 - analogRead(rightLightSensor); // This reads the value of the sensor, then saves it to the corresponding integer.
+  SensorRight = 1023 - analogRead(rightLightSensor);      // This reads the value of the sensor, then saves it to the corresponding integer.
   delay(1);
-  SensorDifference = abs(SensorLeft - SensorRight); // This calculates the difference between the two sensors and then saves it to an integer.
+  SensorDifference = abs(SensorLeft - SensorRight);       // This calculates the difference between the two sensors and then saves it to an integer.
   
   if (proxVal > minDistance) {
     goStop();
@@ -65,72 +68,19 @@ void loop()
     if (SensorLeft < SensorRight && SensorDifference > lightDiff) goLeft();
     if (SensorDifference < lightDiff) goForward();
   }
-  
-//   turnRandom(100,1000);            //Turns randomly left or right for a random time period between .1 second and one second
-//   goForwardRandom(1000,2000);      //Goes forward for a random time period between 1 and 2 seconds
-//   goStop();                        //Stops the robot
-//   delay(2000);                     //pauses for 2 seconds (whilst stopped)
 }
 
-/*
- * turns the robot randomly left or right for a random time period between
- * minTime (milliseconds) and maxTime (milliseconds)
- */
-void turnRandom(int minTime, int maxTime){
-  int choice = random(2);                     //Random number to decide between left (1) and right (0)
-  int turnTime = random(minTime,maxTime);     //Random number for the pause time
-  if(choice == 1){ goLeft();}                 //If random number = 1 then turn left
-  else {goRight();}                           //If random number = 0 then turn right
-  delay(turnTime);                            //delay for random time                         
-}
+// FUNCTIONS
+//--------------------------------------------------------------------------------- 
 
-/*
- * goes forward for a random time period between minTime (milliseconds)
- * and maxTime (milliseconds)
- */
-void goForwardRandom(int minTime, int maxTime){
-  int forwardTime = random(minTime,maxTime);      //determine a random time to go forward
-  goForward();                                    //sets the SERB forward
-  delay(forwardTime);                             //delays for random time period
-}
-
-//------------------------------------------------------------------------------------------------------------
-//START OF ARDUINO CONTROLLED SERVO ROBOT (SERB) ROUTINES
-
-/*
- * sets up your arduino to address your SERB using the included routines
-*/
 void serbSetup(){
-  setSpeed(speed);
   pinMode(LEFTSERVOPIN, OUTPUT);     //sets the left servo signal pin to output
   pinMode(RIGHTSERVOPIN, OUTPUT);    //sets the right servo signal pin to output
   leftServo.attach(LEFTSERVOPIN);    //attaches left servo
   rightServo.attach(RIGHTSERVOPIN);  //attaches right servo
-  pinMode(rightLightSensor, INPUT);
-  pinMode(leftLightSensor, INPUT);
-  goStop();
-}
-
-
-/*
- * sets the speed of the robot between 0-(stopped) and 100-(full speed)
- * NOTE: speed will not change the current speed you must change speed 
- * then call one of the go methods before changes occur.
-*/ 
-void setSpeed(int newSpeed){
-  if(newSpeed >= 100) {newSpeed = 100;}     //if speed is greater than 100 make it 100
-  if(newSpeed <= 0) {newSpeed = 0;}         //if speed is less than 0 make it 0 
-  speed = newSpeed * 0.9;                   //scales the speed to be between 0 and 90
-}
-
-void goRight(){
- leftServo.write(90 + speed);
- rightServo.write(90 - speed);
-}
-
-void goLeft(){
- leftServo.write(90 - speed);
- rightServo.write(90 + speed);
+  pinMode(rightLightSensor, INPUT);  //sets the right light sensor signal pin to input
+  pinMode(leftLightSensor, INPUT);   //sets the left light sensor signal pin to input
+  goStop();                          //stops servos just in case still running
 }
 
 void goForward(){
@@ -143,9 +93,17 @@ void goBackward(){
  rightServo.write(90 - speed);
 }
 
+void goRight(){
+ leftServo.write(90 + speed);
+ rightServo.write(90 - speed);
+}
+
+void goLeft(){
+ leftServo.write(90 - speed);
+ rightServo.write(90 + speed);
+}
+
 void goStop(){
  leftServo.write(90);
  rightServo.write(90);
-} 
-//END OF ARDUINO CONTROLLED SERVO ROBOT (SERB) ROUTINES
-//------------------------------------------------------------------------------------------------------------
+}
